4fcde65b67dc552bdb63ab9b4469a779335a2144
Max <maxtretter@tutanota.com>
2023-06-14 12:34:32 +0200

    add funcionality to delete button

--- shoppingcard.js
+++ shoppingcard.js
@@ -18,7 +18,7 @@ const populateProducts = () => {
         const product = products.find(obj => obj.id === item.id)
         if (product) {
             // if product exists than display it in the DOM
-            displayProduct(product, cardProductContainer, item)
+            displayProduct(product, cardProductContainer, item, cardProductContainer)
             console.log('product:', product);
         } else {
             console.log('Product not availabe')
@@ -26,9 +26,10 @@ const populateProducts = () => {
     })
 };
 
-const displayProduct = (product, productContainer, item) => {
-    // id of current card item
+const displayProduct = (product, productContainer, item, cardProductContainer) => {
+    // variables for multiple use within funtion
     const currentCardItem = cardItems[item.id - 1];
+    const sum = (product.price * item.count).toFixed(2);
     // display image
     const image = document.createElement('img');
     productContainer.append(image);
@@ -39,79 +40,84 @@ const displayProduct = (product, productContainer, item) => {
     header.classList.add('productHeaderCard');
     header.innerHTML = product['header'];
     productContainer.append(header);
-    // empty container
+    // display empty container
     const empty1 = document.createElement('div');
     empty1.innerhtml = ' ';
     productContainer.append(empty1);
-    // Change Quantity
+
+    // display Change Quantity with plus and minus
+    //************************************************************** */
+    //container
     const changeQuantityContainer = document.createElement('div');
     changeQuantityContainer.classList.add('changeQuantityContainer');
     productContainer.append(changeQuantityContainer);
-        // make three items for '+' / 'changequantity' / '-'
-        // decrease button
-        const decreaseQantity = document.createElement('button');
-        decreaseQantity.classList.add('bi', 'bi-dash-circle');
-        changeQuantityContainer.append(decreaseQantity);
-        // eventlistener decrease
-        const decrease = (sum) => {
-            currentCardItem.count = item.count - 1;
-            // change display of quantity in inputfield and show-quantity-field according to increase
-            changeQuantityInput.value = currentCardItem.count;
-            showQuantityContainer.innerHTML = `${currentCardItem.count} x ${product.price} EUR`;
-            sum = (product.price * currentCardItem.count).toFixed(2);
-            showSum.innerHTML = `${sum} EUR`
-        };
-        decreaseQantity.addEventListener('click', decrease);
-        // show quantitychange
-        const changeQuantityInput= document.createElement('input');
-        changeQuantityInput.value = currentCardItem.count;
-        changeQuantityInput.id = 'changeQuantityInput';
-        changeQuantityContainer.append(changeQuantityInput);
-        // increase button
-        const increaseQantity = document.createElement('button');
-        increaseQantity.classList.add('bi', 'bi-plus-circle');
-        changeQuantityContainer.append(increaseQantity);
-        //eventListener increase
-        console.log('item', item)
-        const increase = (sum) => {
-            currentCardItem.count = item.count + 1;
-            // change display of quantity in inputfield and show-quantity-field according to increase
-            changeQuantityInput.value = currentCardItem.count;
-            showQuantityContainer.innerHTML = `${currentCardItem.count} x ${product.price} EUR`;
-            sum = (product.price * currentCardItem.count).toFixed(2);
-            showSum.innerHTML = `${sum} EUR`
-        };
-        increaseQantity.addEventListener('click', increase);
-    // show Quantity and item price
+    // make three items for '+' / 'changequantity' / '-'
+    // decrease button
+    const decreaseQantity = document.createElement('button');
+    decreaseQantity.classList.add('bi', 'bi-dash-circle');
+    changeQuantityContainer.append(decreaseQantity);
+    // eventlistener decrease
+    const decrease = (sum) => {
+        currentCardItem.count = item.count - 1;
+        // change display of quantity in inputfield and show-quantity-field according to increase
+        updateAndDisplaySum(sum);
+    };
+    decreaseQantity.addEventListener('click', decrease);
+    // show quantitychange
+    const changeQuantityInput = document.createElement('input');
+    changeQuantityInput.value = currentCardItem.count;
+    changeQuantityInput.id = 'changeQuantityInput';
+    changeQuantityContainer.append(changeQuantityInput);
+    // increase button
+    const increaseQantity = document.createElement('button');
+    increaseQantity.classList.add('bi', 'bi-plus-circle');
+    changeQuantityContainer.append(increaseQantity);
+    //eventListener increase
+    const increase = (sum) => {
+        currentCardItem.count = item.count + 1;
+        // change display of quantity in inputfield and show-quantity-field according to increase
+        updateAndDisplaySum(sum);
+    };
+    increaseQantity.addEventListener('click', increase);
+
+    // show and change display Quantity and item price
     const showQuantityContainer = document.createElement('div');
     showQuantityContainer.classList.add('showQuantityContainer');
     productContainer.append(showQuantityContainer);
     showQuantityContainer.innerHTML = `${currentCardItem.count} x ${product.price} EUR`;
+
+    //************************************************************** */
     // show sum and delete option
     const showSumAndDeleteOptionContainer = document.createElement('div');
     showSumAndDeleteOptionContainer.classList.add('showSumAndDeleteOptionContainer');
     productContainer.append(showSumAndDeleteOptionContainer);
-        // show sum
-        const showSum = document.createElement('p');
-        showSum.id = 'showSum';
-        const sum = (product.price * item.count).toFixed(2);
+    // show sum
+    const showSum = document.createElement('p');
+    showSum.id = 'showSum';
+
+    showSum.innerHTML = `${sum} EUR`
+    showSumAndDeleteOptionContainer.append(showSum);
+    // delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('bi', 'bi-trash')
+    showSumAndDeleteOptionContainer.append(deleteBtn);
+
+    const deleteItem = () => {
+        currentCardItem.count = 0;
+        console.log('currentCardItem.count', currentCardItem.count)
+        updateAndDisplaySum();
+
+    };
+    deleteBtn.addEventListener('click', deleteItem);
+
+
+    const updateAndDisplaySum = (sum) => {
+        sum = (product.price * currentCardItem.count).toFixed(2);
         showSum.innerHTML = `${sum} EUR`
-        showSumAndDeleteOptionContainer.append(showSum);
-        // delete button
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add('bi', 'bi-trash')
-        showSumAndDeleteOptionContainer.append(deleteBtn);
-
-
-
-    // display price
-    // const price = document.createElement('p');
-    // price.classList.add('priceCard');
-    // price.innerHTML = `${product["price"]} EUR`
-    // productContainer.append(price);
-
-    // change quantity dropdown
-    // remove button
+        showQuantityContainer.innerHTML = `${currentCardItem.count} x ${product.price} EUR`;
+        changeQuantityInput.value = currentCardItem.count;
+        cardProductContainer.remove();
+    }
 };
 
 
@@ -126,6 +132,3 @@ document.addEventListener('DOMContentLoaded', init);
 // show the purchase quantity of the id
 // show the Euro sum of quantity and price
 // after the loop show the sum of purchase
-
-
-
